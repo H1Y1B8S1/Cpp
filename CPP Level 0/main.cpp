b002be822c74d6e048f4075687198404cd951a4b
Vishwajitsinhdodiya <dodiyavishwajit@gmail.com>
2023-12-23 22:09:30 +0530

    conflit solved

--- CPP Level 0/main.cpp
+++ CPP Level 0/main.cpp
@@ -5,12 +5,9 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-<<<<<<< HEAD
     std ::cout << "jay Shree Ram bhaiyo....";
-=======
     std ::cout << "jay Shree Ram....";
     std ::cout << "jay Shree Ram....";
->>>>>>> 9d37c29964758134b3f080619abc7e45adb2844b
     getch();
     return 0;
 
