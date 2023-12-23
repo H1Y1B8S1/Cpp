1fb4f2f7035cfffc701f4a818dc67d0d21504bdb
Siddharthsinh Dodiya <siddharthsinh@immersivequest.ai>
2023-12-23 21:11:28 +0530

    new file has been created.

---
+++ CPP Level 0/New.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    char arr[] = "Welcome to C++ Level 0";
+    cout << "Value of ary is:" << endl
+         << arr << ends;
+    cout <<"\n";
+
+    return 0;
+}
