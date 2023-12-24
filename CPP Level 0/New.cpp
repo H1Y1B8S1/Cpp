aca97d8d418011d5d3f73709ccd987e6115a58ac
Siddharthsinh Dodiya <siddharthsinh@immersivequest.ai>
2023-12-24 18:37:46 +0530

    new.cpp file chagned

--- CPP Level 0/New.cpp
+++ CPP Level 0/New.cpp
@@ -3,18 +3,6 @@ using namespace std;
 
 int main()
 {
-    int factorial();
-
-    factorial();
-    factorial();
-    factorial();
+    
 }
 
-int factorial()
-{
-    static int i = 0;
-    int j = 0;
-    i++, j++;
-
-    cout << i << " " << j << endl;
-}
