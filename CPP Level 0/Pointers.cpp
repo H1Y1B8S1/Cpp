5924993c96c5a9707f8282f4eab8ce051b6e90bb
Siddharthsinh Dodiya <siddharthsinh@immersivequest.ai>
2023-12-24 19:14:47 +0530

    pointer file updated.

--- CPP Level 0/Pointers.cpp
+++ CPP Level 0/Pointers.cpp
@@ -1,3 +1,20 @@
+/*
+Advantage of pointer
+    1) Pointer reduces the code and improves the performance, it is used to retrieving strings,
+        trees etc. and used with arrays, structures and functions.
+    2) We can return multiple values from function using pointer.
+    3) It makes you able to access any memory location in the computer's memory.
+Usage of pointer
+There are many usage of pointers in C++ language.
+    1) Dynamic memory allocation
+    In c language, we can dynamically allocate memory using malloc() and calloc() functions where
+        pointer is used.
+    2) Arrays, Functions and Structures
+
+Pointers in c language are widely used in arrays, functions and structures.
+It reduces the code and improves the performance.
+*/
+
 #include <iostream>
 using namespace std;
 
@@ -6,16 +23,27 @@ void PointeR(); // We have to declare just a fun without a body, above main to u
                 // Or we could use Main at the end of all functions. so no need any declaraions.
 void Boolens();
 
+void PointerExample()
+{
+    int number = 30;
+    int *p;      // pointer initialization
+    p = &number; // stores the address of number variable
+    cout << "Address of number variable is:" << &number << endl;
+    cout << "Address of p variable is:" << p << endl;
+    cout << "Value of p variable is:" << *p << endl;
+}
+
 int main(int argc, char const *argv[])
 {
     PointeR();
     Boolens();
+    PointerExample();
     return 0;
 }
 
 void PointeR()
 {
-    int *ptr;                   // pointer declaration
+    int *ptr;                   // pointer declaration --> ptr can point to an address which holds int data
     int a[] = {10, 20, 30, 40}; // array initialization
 
     ptr = a; // assigning base address of array to the pointer ptr
