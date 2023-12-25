7fcc38ce1b14b1833ae45b7c7eb1d2d1af65f3ea
h1y1b8s1 <h1y1b8s1@gmail.com>
2023-12-25 11:06:08 +0530

    New Class.cpp file created for understandig of OPPs concepts.

--- CPP Level 0/Class.cpp
+++ CPP Level 0/Class.cpp
@@ -3,14 +3,30 @@ using namespace std;
 
 class Student
 {
-public:
-    int n = 9;
+    // private field (both variables)
+    int id = 9;
     string name = "siddharthsinh";
+
+public:
+    // Seting both value by calling this method.
+    void SetIdName(int i, string nam)
+    {
+        id = i,
+        name = nam;
+    }
+    // This method will return default value of id and name if you dont set new values.
+    void GetIdName()
+    {
+        cout << "ID: " << id << "\nname: " << name << endl;
+    }
 };
 
 int main(int argc, char const *argv[])
 {
     Student obj;
-    cout << obj.name << endl;
+    obj.GetIdName();
+    obj.SetIdName(3, "sidhu");
+    obj.GetIdName();
+
     return 0;
 }
