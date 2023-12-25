8129bc48e94d1920fb24c82cd70eea8bed13774d
h1y1b8s1 <h1y1b8s1@gmail.com>
2023-12-25 10:15:23 +0530

    adding new file class

--- CPP Level 0/Class.cpp
+++ CPP Level 0/Class.cpp
@@ -3,5 +3,14 @@ using namespace std;
 
 class Student
 {
-
+public:
+    int n = 9;
+    string name = "siddharthsinh";
 };
+
+int main(int argc, char const *argv[])
+{
+    Student obj;
+    cout << obj.name << endl;
+    return 0;
+}
