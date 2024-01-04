07d106e05701e5c4ab0f8d939951c86736e77021
H1Y1B8S1 <h1y1b8s1@gmail.com>
2024-01-04 23:46:21 +0530

    New folder for the visual studio practice

---
+++ VisualStudioClassPractice/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "main.h"
+
+int main()
+{
+	std::cout << "Jay shree Ram!!";
+
+	BankAccount AC1("Siddharth", 1000);
+	BankAccount AC2("Sidd", 2000);
+
+	AC1.deposit(500);
+	AC2.withdraw(500);
+	AC1.displayBalance();
+	AC2.displayBalance();
+
+}
+
