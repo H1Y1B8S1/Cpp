7f797a75ee35b79549f4e51a739838021f8174df
H1Y1B8S1 <h1y1b8s1@gmail.com>
2024-02-22 10:18:23 +0530

    d

--- CPP Level 0/main.cpp
+++ CPP Level 0/main.cpp
@@ -1,13 +1,15 @@
-#include <iostream> 
-#include <conio.h>  // getch()
+#include <iostream>
+#include <conio.h> // getch()
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    std ::cout << "jay Shree Ram bhaiyo...."<<endl;
-    std ::cout << "jay Shree Ram...."<<endl;
-    std ::cout << "jay Shree Ram...."<<endl;
+    std ::cout << "jay Shree Ram bhaiyo...." << endl;
+    std ::cout << "jay Shree Ram...." << endl;
+    std ::cout << "jay Shree Ram...." << endl;
+
+    std::cout << "har har mahadev" << endl;
     getch();
     return 0;
 
