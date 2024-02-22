01bb99f4d0c91761ef9f5e3daacd77e4365b92f7
H1Y1B8S1 <h1y1b8s1@gmail.com>
2024-02-22 10:33:19 +0530

    settings

--- Cpp Level 1/accumulate/accumulate.cpp
+++ Cpp Level 1/accumulate/accumulate.cpp
@@ -22,7 +22,7 @@ accumulator value. The result is an accumulated value.
 int main()
 {
     std::vector<int> numbers = {1, 2, 3, 4, 5}; // sum = 15
-    int sum = std::accumulate(numbers.begin(), numbers.end(), /*inial value is 3*/ 3);
+    int sum = std::accumulate(numbers.begin(), numbers.end(), *inial value is 3*/ 3);
     std::cout << "Sum of elements: " << sum << std::endl; // sum it will return 15+3 = 18
 
     // Using std::accumulate with a lambda function to square each element and add them together
