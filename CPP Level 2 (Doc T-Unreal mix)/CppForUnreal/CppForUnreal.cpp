952e133749f4b28ce1e958d72aeb635c06253ca2
H1Y1B8S1 <h1y1b8s1@gmail.com>
2024-03-05 11:22:41 +0530

    day 2

--- CPP Level 2 (Doc T-Unreal mix)/CppForUnreal/CppForUnreal.cpp
+++ CPP Level 2 (Doc T-Unreal mix)/CppForUnreal/CppForUnreal.cpp
@@ -1,6 +1,3 @@
-// CppForUnreal.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 //java dot comment
@@ -11,17 +8,11 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    
+	const int SecondsPerMinutes{ 60 };
+	int TotalSecondPlayed{ 100 };
+
+
+	std::cout << TotalSecondPlayed / SecondsPerMinutes << " Minutes and " << TotalSecondPlayed % SecondsPerMinutes << " Seconds\n";
+	// float type cast
+	std::cout << static_cast<float>(TotalSecondPlayed)/SecondsPerMinutes<<" Minutes\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln fil
