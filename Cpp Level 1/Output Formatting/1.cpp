3a0f384269f6426ca30942ac9b90fd19b6a3b87e
H1Y1B8S1 <h1y1b8s1@gmail.com>
2024-03-13 15:32:38 +0530

    output field

---
+++ Cpp Level 1/Output Formatting/1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+
+int main(){
+
+    std::cout<<"siddharth"<<" "<<"dodiya"<<" "<<"09"<<"\n";
+    std::cout<<"siddharth"<<" "<<"dodiya"<<" "<<"09"<<"\n";
+    std::cout<<"siddharth"<<" "<<"dodiya"<<" "<<"09"<<"\n";
+    std::cout<<"siddharth"<<" "<<"dodiya"<<" "<<"09"<<"\n";
+
+    std::cout<<"\nsorted table\n\n";
+
+    std::cout<<std::setw(10)<<"siddharth"<<std::setw(10)<<"dodiya"<<std::setw(10)<<"09"<<"\n";
+    std::cout<<std::setw(10)<<"Ajay"<<std::setw(10)<<"alex"<<std::setw(10)<<"03"<<"\n";
+    std::cout<<std::setw(10)<<"Vishwajit"<<std::setw(10)<<"op"<<std::setw(10)<<"03"<<"\n";
+    std::cout<<std::setw(10)<<"Ram"<<std::setw(10)<<"scope"<<std::setw(10)<<"06"<<"\n";
+
+    return 0;
+}
