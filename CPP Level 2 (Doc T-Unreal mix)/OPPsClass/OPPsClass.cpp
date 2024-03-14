f5219a0ffc1e3d61a0943e6355512ea3659f7b22
H1Y1B8S1 <h1y1b8s1@gmail.com>
2024-03-14 17:44:21 +0530

    advance method

--- CPP Level 2 (Doc T-Unreal mix)/OPPsClass/OPPsClass.cpp
+++ CPP Level 2 (Doc T-Unreal mix)/OPPsClass/OPPsClass.cpp
@@ -28,3 +28,47 @@ int main()
 	Employee employee1("Sid", "IQ", 24);
 	employee1.IntroduceYourself();
 }
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//#include <iostream>
+//#include <string> // Include string header for std::string
+//
+//class Employee
+//{
+//private:
+//	std::string Name;
+//	std::string Company;
+//	int Age;
+//
+//public:
+//	// Constructor with member initializer list
+//	Employee(const std::string& name, const std::string& company, int age)
+//		: Name(name), Company(company), Age(age)
+//	{
+//	}
+//
+//	// Getter methods for encapsulation
+//	const std::string& getName() const { return Name; }
+//	const std::string& getCompany() const { return Company; }
+//	int getAge() const { return Age; }
+//
+//	// Method to introduce the employee
+//	void introduceYourself() const
+//	{
+//		std::cout << "Name: " << Name << "\n"
+//			<< "Company: " << Company << "\n"
+//			<< "Age: " << Age << "\n";
+//	}
+//};
+//
+//int main()
+//{
+//	// Create an employee object
+//	Employee employee1("Sid", "IQ", 24);
+//
+//	// Call the introduce method
+//	employee1.introduceYourself();
+//
+//	return 0;
+//}
