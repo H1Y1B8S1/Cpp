47b29761200a677ad07637bba531cbc78fbc9042
H1Y1B8S1 <h1y1b8s1@gmail.com>
2024-05-02 19:39:15 +0530

    minor

--- Cpp Level 1/Locationlib_VStudio/Locationlib_VStudio.cpp
+++ Cpp Level 1/Locationlib_VStudio/Locationlib_VStudio.cpp
@@ -1,32 +1,40 @@
-#include <iostream>
-#include "Locationlib.h"
+#include <iostream>  // Include necessary header file for input/output operations
+#include "Locationlib.h"  // Include your custom header file
 
+// Define a constant for enemy view radius
 constexpr float enemy_view_radius_meter = 5;
 
+// Main function where the program execution starts
 int main()
 {
-	std::array<float, 3> enemy1_location = {2, 2, 0};
-	std::array<float, 3> enemy2_location = {2, 4, 0};
+    // Define the locations of two enemies as 3D points using std::array
+    std::array<float, 3> enemy1_location = { 2, 2, 0 };
+    std::array<float, 3> enemy2_location = { 2, 4, 0 };
 
-	float enemy_distance = sid::locationlib::distance(enemy1_location, enemy2_location);
-	float distance_from_center = sid::locationlib::distance(enemy1_location);
+    // Calculate the distance between two enemies using the distance function from Locationlib
+    float enemy_distance = sid::locationlib::distance(enemy1_location, enemy2_location);
 
-	using sid::locationlib::circumference;
-	//float view_circumference_for_enemy = sid::locationlib::circumference(enemy_view_radius_meter); --> both methods are same
-	float view_circumference_for_enemy = circumference(enemy_view_radius_meter);
+    // Calculate the distance of enemy1 from the origin (0, 0, 0) using the distance function
+    float distance_from_center = sid::locationlib::distance(enemy1_location);
 
-	float total_distance = sid::locationlib::total_walking_distance({ enemy1_location,
-																	{2, 3, 0}, // y += 1
-																	{2, 3, 3}, // z += 3
-																	{5, 3, 3}, // x += 3
-																	{8, 3, 3}, // x += 3
-																	{8, 3, 2}, // z -= 1
-																	{2, 3, 2}, // x -= 6
-																	{2, 3, 1}, // z -= 1
-																	{2, 3, 0}, // z -= 1
-																	enemy2_location });
+    // Use the circumference function from Locationlib to calculate the view circumference for the enemy
+    using sid::locationlib::circumference;
+    const float view_circumference_for_enemy = circumference(enemy_view_radius_meter);
 
-	std::cout << "The two enemies are " << enemy_distance << "m apart and can see for a circumference of "
-		<< view_circumference_for_enemy << "m. To go to from one to the other they need to walk "
-		<< total_distance << "m.";
+    // Calculate the total walking distance based on a sequence of 3D points using total_walking_distance function
+    const float total_distance = sid::locationlib::total_walking_distance({ enemy1_location,
+                                                                      {2, 3, 0}, // y += 1
+                                                                      {2, 3, 3}, // z += 3
+                                                                      {5, 3, 3}, // x += 3
+                                                                      {8, 3, 3}, // x += 3
+                                                                      {8, 3, 2}, // z -= 1
+                                                                      {2, 3, 2}, // x -= 6
+                                                                      {2, 3, 1}, // z -= 1
+                                                                      {2, 3, 0}, // z -= 1
+                                                                      enemy2_location });
+
+    // Output the calculated distances and circumferences
+    std::cout << "The two enemies are " << enemy_distance << "m apart and can see for a circumference of "
+        << view_circumference_for_enemy << "m. To go from one to the other they need to walk "
+        << total_distance << "m.";
 }
