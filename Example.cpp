b145ba3050ff31849b56636724ca2525fa868ec7
H1Y1B8S1 <h1y1b8s1@gmail.com>
2024-05-04 08:36:31 +0530

    working on level 1 oops

--- Example.cpp
+++ Example.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 
+void callbyvalue(int  value){
+    value +=5;
+    std::cout<<value<<"\n";
+}
+
+void callbyreference(int& value){
+    value+=5;
+    std::cout<<value<<"\n";
+
+}
+
+
 int main()
 {
+    int value = 5;
+    callbyvalue(value);
+    std::cout<<value<<"\n";
+    callbyreference(value);
+    std::cout<<value<<"\n";
 
-    std::cout << 134 % 2;
 
     return 0;
 }
