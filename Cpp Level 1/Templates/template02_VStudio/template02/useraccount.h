21641d402bbe5c668328509316f27eb22a8da737
H1Y1B8S1 <h1y1b8s1@gmail.com>
2024-05-07 10:49:59 +0530

    WORKING WITH templates

---
+++ Cpp Level 1/Templates/template02_VStudio/template02/useraccount.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+#include <array>
+#include <iostream>
+
+class useraccount
+{
+private:
+	int userID;
+	std::string email;
+public:
+	useraccount(int id,std::string email):userID(id),email(email){}
+
+	//Serialize user account by bytes
+	static std::array<char, 100> serialize(const useraccount& account);
+
+	//Construct an user account from bytes
+	static useraccount deserialize(const std::array<char, 100>& blob);
+
+	//Print the useracount instant
+	friend std::ostream& operator<<(std::ostream& os,const useraccount& account);
+ };
+
