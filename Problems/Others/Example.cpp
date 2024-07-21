3ca5cd7d20ffd9f003b60b36c1f6389dfe2052d2
H1Y1B8S1 <h1y1b8s1@gmail.com>
2024-07-21 17:49:58 +0530

    exm

--- Problems/Others/Example.cpp
+++ Problems/Others/Example.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-int main()
+
+class Solution
 {
-    std::cout << 2 * 2;
+public:
+    vector<int> selfDividingNumbers(int left, int right)
+    {
+        vector<int> op = {};
+        for (int i = left; i <= right; i++)
+        {
+            vector<int> temp = {};
+            int num = i;
+            bool flag = true;
+
+            while (num != 0)
+            {
+                int digi = num % 10;
+
+                temp.push_back(digi);
+                num /= 10;
+            }
+            std::cout << "->";
+
+            for (int d : temp)
+            {
+                std::cout << d << " ";
+                if (d == 0 || i % d != 0)
+                    flag = false;
+            }
+            if (flag)
+            {
+                op.push_back(i);
+            }
+        }
+        return op;
+    }
+};
+
+int main(void)
+{
+    Solution sol;
+    vector<int> op = sol.selfDividingNumbers(1, 22);
+    for (int i : op)
+    {
+        std::cout << i << " ";
+    }
     return 0;
 }
