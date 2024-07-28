dbf5d635c6536841e5a3c9fcc62c9f4f377b1b83
H1Y1B8S1 <h1y1b8s1@gmail.com>
2024-07-28 18:18:01 +0530

    bitsety

--- Cpp/A/binary_bitset.cpp
+++ Cpp/A/binary_bitset.cpp
@@ -1,11 +1,22 @@
-#include<iostream>
-#include<bitset>
+#include <iostream>
+#include <bitset>
 
-int main() {
-    int number = 42; 
-    std::bitset<32> binary(number); 
+int main()
+{
+    int number = 42;
+    std::bitset<32> binary(number);
 
     std::cout << "32 bit Binary representation of " << number << " is " << binary << std::endl;
+
+    std::string binaryString = "001110011100111001110011100111";
+
+    // Ensure the size of the bitset matches the length of the binary string
+    std::bitset<32> bitsetValue(binaryString);
+
+    // Convert bitset to unsigned long
+    unsigned long decimalValue = bitsetValue.to_ulong();
+
+    std::cout << "Decimal value: " << decimalValue << std::endl;
 
     return 0;
 }
