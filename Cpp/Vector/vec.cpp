5ee50b08aad4649b14a9dadb48352297b6d5507e
H1Y1B8S1 <h1y1b8s1@gmail.com>
2024-08-03 17:56:19 +0530

    vector2

--- Cpp/Vector/vec.cpp
+++ Cpp/Vector/vec.cpp
@@ -13,17 +13,25 @@ int main(void)
     // #[01] Vector Initialization
     std::vector<int> nums = {10, 20, 30};             // Initialization with a list of values
     std::vector<int> nums2(nums);                     // Copy constructor
-    std::vector<int> nums3(nums.begin(), nums.end()); // 3. Range constructor
+    std::vector<int> nums3(nums.begin(), nums.end()); // Range constructor
     std::vector<int> nums4;                           // Default constructor
     nums4 = nums;
     std::vector<int> nums5(5, 10);            // A vector of size 5, all the values are 10
     std::vector<int> nums6(std::move(nums4)); // Move constructor
 
     // #[02] Adding elements
-    nums.push_back(40);
-    nums.emplace_back(50);
-    nums.insert(nums.begin() + 2, 99);
-    nums2.insert(nums2.end(), nums.begin(), nums.end());
+    nums.push_back(40);                                  // Adds an element to the end of the vector
+    nums.emplace_back(50);                               // Constructs and adds an element to the end of the vector
+    nums.insert(nums.begin(), 1);                        // Inserts an element at the beginning
+    nums.insert(nums.begin() + 2, 100);                  // Inserts an element at a specified position
+    nums.insert(nums.end(), 60);                         // Inserts an element at the end
+    nums.insert(nums.begin() + 1, {70, 80, 90});         // Inserts a list of elements at a specified position
+    nums2.insert(nums2.end(), nums.begin(), nums.end()); // Inserts a range of elements at the end of the vector
 
+    for (int num : nums)
+    {
+        std::cout << num << ",";
+    }
+    std::cout << std::endl;
     return 0;
 }
